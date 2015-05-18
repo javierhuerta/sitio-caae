ae172b3fe0d8469aef606ae2de4ab81da042c148
Javier Huerta <javierhuerta91@gmail.com>
2015-05-18 00:26:48 -0300

    fix

--- webpage/templatetags/webpage_tags.py
+++ webpage/templatetags/webpage_tags.py
@@ -1,5 +1,5 @@
 from django import template
-from webpage.models import Menu
+from webpage.models import Menu, HomePage
 
 register = template.Library()
 
@@ -10,3 +10,10 @@ def navbar_menu(context):
     'menus': Menu.objects.all().order_by('order'),
     'request': context['request'],
   }
+
+@register.assignment_tag(takes_context=True)
+def get_site_root(context):
+    # NB this returns a core.Page, not the implementation-specific model used
+    # so object-comparison to self will return false as objects would differ
+    id = context['request'].site.root_page.id
+    return HomePage.objects.get(id=id)
